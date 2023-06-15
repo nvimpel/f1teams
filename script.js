1382e293adb5d7f4d025b737ab03b05b771be81c
nvimpel <nvimpel@gmail.com>
2023-06-16 00:06:53 +0200

    Movable button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var LastSize;
 var LastMarginTop;
 var LastMarginLeft;
+var LastOffset;
 function sizeUp(Name)
 {
     LastSize = document.getElementById(Name).style.width
@@ -16,4 +17,24 @@ function sizeDown(Name)
     document.getElementById(Name).style.width = LastSize;
     document.getElementById(Name).style.marginTop = LastMarginTop;
     document.getElementById(Name).style.marginTop = LastMarginLeft;
+}
+var moveInterval;
+function moveAway(Name)
+{
+    
+    moveInterval = setInterval(function() {
+    var div = document.getElementById(Name);
+    div.style.left = div.offsetLeft - 10 + "px"; },
+    10);
+    
+}
+function snapBack(Name)
+{
+    clearInterval(moveInterval);
+    document.getElementById(Name).style.left = 0+'px';
+}
+function savePosition(Name)
+{
+    LastOffset = document.getElementById(Name).style.offsetLeft = 0;
+    console.log(LastOffset);
 }
